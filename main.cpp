92cc8da4c204216ba870422a24e9eee0a9a90780
socgerlia <socgerlia@gmail.com>
2015-12-23 01:02:36 +0800

    [osx] modify CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -146,11 +146,31 @@ void test1(){
 	DM(str)
 }
 
+#include <boost/network/protocol/http/client.hpp>
+
+int test2(int argc, char** argv){
+	using namespace boost::network;
+
+	if (argc != 2){
+		std::cout << "Usage: " << argv[0] << " [url]" << std::endl;
+		return 1;
+	}
+
+	http::client client;
+	http::client::request request(argv[1]);
+	request << header("Connection", "close");
+	http::client::response response = client.get(request);
+	std::cout << body(response) << std::endl;
+
+	return 0;
+}
+
 
 int main(int argc, char** argv){
 	game_table gt;
 
-	test0();
+	// test0();
+	test2(argc, argv);
 	return 0;
 }
 
